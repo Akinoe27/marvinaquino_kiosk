5897ab57cb3ba70e42ddbf0978c8363851c7e3b0
Akinoe27 <elder_aquino27@yahoo.com>
2024-01-04 15:41:13 +0800

    update

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,10 +1,8 @@
-from flaskapp import app
+from flaskapp import app, mysql
 from flask import render_template
 
 @app.route('/')
 def index():
-    @app.route('/')
-    def index():
        cursor = mysql.connection.cursor()
        cursor.execute("SELECT * FROM products")  # Assuming 'products' is your table name
        products = cursor.fetchall()
