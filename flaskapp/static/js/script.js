f1b606695fec4922f44fcea696f7b8b2b61c67d1
Akinoe27 <elder_aquino27@yahoo.com>
2024-01-06 11:19:32 +0800

    update

--- flaskapp/static/js/script.js
+++ flaskapp/static/js/script.js
@@ -13,3 +13,26 @@ function addItemToCart(price) {
 function showImageinModal(imageUrl) {
   document.getElementById("modalImage").src = imageUrl;
 }
+
+// modal for order
+function setupModal(button) {
+  const orderModal = document.getElementById("orderModal");
+  const productNameInput = orderModal.querySelector("#productName");
+  const quantityInput = orderModal.querySelector("#quantity");
+  const totalPriceInput = orderModal.querySelector("#totalPrice");
+  const productImage = orderModal.querySelector("#productImage");
+
+  productNameInput.value = button.dataset.name;
+  productImage.src = button.dataset.image;
+  const price = parseFloat(button.dataset.price);
+  const unit = button.dataset.unit;
+  quantityInput.value = unit === "kg" ? "1.0" : "1";
+  quantityInput.step = unit === "kg" ? "0.01" : "1";
+  totalPriceInput.value = price.toFixed(2);
+
+  quantityInput.oninput = () => {
+    totalPriceInput.value = (parseFloat(quantityInput.value) * price).toFixed(
+      2
+    );
+  };
+}
